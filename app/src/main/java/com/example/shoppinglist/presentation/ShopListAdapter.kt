b0dc29ad562ffa4b5be39a8de74755bbcaecb70a
feltsbar <kuzmin.max6569@gmail.com>
2022-02-10 21:47:00 +0300

    added method onViewRecycled to ShopListAdapter so fixed bug with display new item in RecyclerView

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
@@ -26,17 +27,29 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
         val shopItem = shopList[position]
-        holder.tvName.text = shopItem.name
+        val status = if (shopItem.enabled){ "Active" } else { "Not active" }
+        holder.tvName.text = "${shopItem.name} $status"
         holder.tvCount.text = shopItem.count.toString()
         holder.view.setOnLongClickListener {
             true
         }
+        if (shopItem.enabled){
+            holder.tvName.setTextColor(ContextCompat
+                .getColor(holder.view.context, android.R.color.darker_gray))
+        }
+    }
+
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.tvName.text = ""
+        holder.tvCount.text = ""
+        holder.tvName.setTextColor(ContextCompat
+            .getColor(holder.view.context, android.R.color.white))
     }
 
     override fun getItemCount(): Int {
         return shopList.size
     }
-
 
     class ShopItemViewHolder(val view : View) : RecyclerView.ViewHolder(view){
         val tvName = view.findViewById<TextView>(R.id.tv_name)
