eddb3b74377b948999b9f962b125cc3d33726acf
feltsbar <kuzmin.max6569@gmail.com>
2022-02-17 20:53:19 +0300

    began to fill ShopItemActivity, wrote methods parseIntent() and initViews() and fill companion object. I should realized work screen: describe methods launchAddMode() and launchEditMode()

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -4,32 +4,143 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputLayout
+import java.lang.RuntimeException
+import kotlin.jvm.internal.Intrinsics
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName : TextInputLayout
+    private lateinit var tilCount : TextInputLayout
+    private lateinit var etName : EditText
+    private lateinit var etCount : EditText
+    private lateinit var buttonSave : Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
 
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("ShopItemActivity", mode.toString())
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+        when(screenMode){
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
     }
 
-    companion object{
+    private fun launchEditMode(){
+        // TODO: just do it
+    }
+
+    private fun launchAddMode(){
+        etName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                tilName.error = null
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                tilName.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (etName.text.isBlank()){
+                    tilName.error = getString(R.string.til_field_empty_error)
+                }
+            }
+        })
+
+        etCount.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                tilCount.error = null
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                tilCount.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString().isBlank()){
+                    tilCount.error = getString(R.string.til_field_empty_error)
+                }
+//                if (etCount.toString().toInt() < 0 ){
+//                    tilCount.error = getString(R.string.til_field_value_less_than_zero)
+//                }
+            }
+        })
+
+        val name : String = etName.toString()
+        val count : String = etCount.toString()
+
+        buttonSave.setOnClickListener {
+            if (etName.toString().isNotBlank() && etCount.toString().isNotBlank() &&
+            tilName.error == null && tilCount.error == null){
+                viewModel.addShopItem(name, count)
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+                //TODO: Check ShopItemViewModel.addShopItem() with debug. IS it work successful ?
+            } else {
+                Toast.makeText(this,
+                    getString(R.string.toast_fill_all_fields), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun parseIntent(){
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Parameters screen mode is absent")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+
+        if (mode != MODE_EDIT && mode != MODE_ADD ){
+            throw RuntimeException("Unknown screen mode: $mode")
+        }
+        screenMode = mode
+
+        if (screenMode == MODE_EDIT ) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Parameter shop item id is absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    private fun initViews(){
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        buttonSave = findViewById(R.id.save_button)
+    }
+
+    companion object {
         private const val EXTRA_SCREEN_MODE = "extra_mode"
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
         private const val MODE_EDIT = "mode_edit"
-        private const val MODE_ADD= "mode_add"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
 
-        fun newIntentAddItem(context: Context): Intent{
+
+        fun newIntentAddItem(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
             return intent
         }
 
-        fun newIntentEditItem(context: Context, shopItemId : Int): Intent{
+        fun newIntentEditItem(context: Context, shopItemId : Int): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
             intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
